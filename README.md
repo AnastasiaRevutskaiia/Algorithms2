7c73334e9d28ee8734e6e4c93bb383fa45d3127b
Alex Ilyenko <aleksey.ilyenko@gmail.com>
2019-08-13 16:44:21 -0400

    Add findSeam and removeSeam (both vertical and horizontal ones) methods

--- README.md
+++ README.md
@@ -5,6 +5,29 @@ Code for programming assignments in Java from the Coursera course, [Algorithms P
 
 Week 1 - Directed and Undirected Graphs
 --------------------------------
- - [**WordNet.java**](https://github.com/alexilyenko/Algorithms2/blob/master/src/main/java/assignment1/WordNet.java) - is a semantic lexicon for the English language that is used extensively by computational linguists and cognitive scientists; for example, it was a key component in IBM's [Watson](http://en.wikipedia.org/wiki/Watson_(computer)). WordNet groups words into sets of synonyms called *synsets* and describes semantic relationships between them. One such relationship is the *is-a* relationship, which connects a *hyponym* (more specific synset) to a *hypernym* (more general synset). For example, *animal* is a hypernym of both *bird* and *fish*; *bird* is a hypernym of *eagle*, *pigeon*, and *seagull*.
+ - [**WordNet.java**](src/main/java/assignment1/WordNet.java) - is a semantic lexicon for the English language that is used extensively by computational linguists 
+ and cognitive scientists; for example, it was a key component in IBM's [Watson](http://en.wikipedia.org/wiki/Watson_(computer)). 
  
-Requires the algs4.jar to run, available in Maven repository. See given [**build.gradle**](https://github.com/alexilyenko/Algorithms2/blob/master/build.gradle) file.
+ WordNet groups words into sets of synonyms called *synsets* and describes semantic relationships between them. 
+ One such relationship is the *is-a* relationship, which connects a *hyponym* (more specific synset) to a *hypernym* (more general synset). 
+ For example, *animal* is a hypernym of both *bird* and *fish*; *bird* is a hypernym of *eagle*, *pigeon*, and *seagull*.
+
+Week 2 - Minimum Spanning Trees and Shortest Paths
+--------------------------------
+ - [**SeamCarver.java**](src/main/java/assignment2/SeamCarver.java) - is a data type that re-sizes a W-by-H image using the seam-carving technique. 
+ 
+ Seam-carving is a content-aware image resizing technique where the image is reduced in size by one pixel of height (or width) at a time. 
+ A vertical seam in an image is a path of pixels connected from the top to the bottom with one pixel in each row; a horizontal seam is a path of pixels connected 
+ from the left to the right with one pixel in each column. Below left is the original 505-by-287 pixel image; below right is the result after removing 150 vertical seams, 
+ resulting in a 30% narrower image. Unlike standard content-agnostic resizing techniques (such as cropping and scaling), seam carving preserves the most interest 
+ features (aspect ratio, set of objects present, etc.) of the image.
+ 
+ ![Original image][1]     ![Seam carved image][2] 
+ 
+ [1]: src/main/resources/assignment2/HJoceanSmall.png (Original image)
+ [2]: src/main/resources/assignment2/HJoceanSmallShrunk.png (Seam carved image)
+ 
+ Although the [underlying algorithm](https://www.youtube.com/watch?v=6NcIJXTlugc) is simple and elegant, it was not discovered until 2007. Now, it is now a core feature in Adobe Photoshop and other computer graphics applications.
+ 
+ --------------------------------
+Requires the algs4.jar to run, available in Maven repository. See given [**build.gradle**](build.gradle) file.
--- src/main/java/assignment2/SeamCarver.java
+++ src/main/java/assignment2/SeamCarver.java
@@ -1,14 +1,16 @@
 package assignment2;
 
 import edu.princeton.cs.algs4.DirectedEdge;
-import edu.princeton.cs.algs4.Edge;
 import edu.princeton.cs.algs4.Picture;
 
-import java.awt.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
 
 public class SeamCarver {
     private static final double BORDER_VALUE = 1000;
     private Picture picture;
+    private boolean isTransposed = false;
 
 
     // create a seam carver object based on the given picture
@@ -21,97 +23,199 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
+        if (isTransposed) {
+            return new Picture(transpose(picture));
+        }
         return new Picture(picture);
     }
 
     // width of current picture
     public int width() {
+        if (isTransposed) {
+            return picture.height();
+        }
         return picture.width();
     }
 
     // height of current picture
     public int height() {
+        if (isTransposed) {
+            return picture.width();
+        }
         return picture.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1) {
-            return BORDER_VALUE;
+        if (isTransposed) {
+            // noinspection SuspiciousNameCombination
+            return energy(picture, y, x);
         }
-        return Math.sqrt(delta(x - 1, y, x + 1, y) + delta(x, y - 1, x, y + 1));
+        return energy(picture, x, y);
     }
 
-    private double delta(int x1, int y1, int x2, int y2) {
-        Color c1 = picture.get(x1, y1);
-        Color c2 = picture.get(x2, y2);
-        int red = c2.getRed() - c1.getRed();
-        int green = c2.getGreen() - c1.getGreen();
-        int blue = c2.getBlue() - c1.getBlue();
+    private double energy(Picture somePicture, int x, int y) {
+        if (x < 0 || x >= somePicture.width() || y < 0 || y >= somePicture.height()) {
+            throw new IllegalArgumentException("x should be in the range [0, width), y should be in the range [0, height)");
+        }
+        if (x == 0 || y == 0 || x == somePicture.width() - 1 || y == somePicture.height() - 1) {
+            return BORDER_VALUE;
+        }
+        return Math.sqrt(delta(somePicture, x - 1, y, x + 1, y) + delta(somePicture, x, y - 1, x, y + 1));
+    }
+
+    private double delta(Picture somePicture, int x1, int y1, int x2, int y2) {
+        int rgb1 = somePicture.get(x1, y1).getRGB();
+        int rgb2 = somePicture.get(x2, y2).getRGB();
+
+        int red1 = (rgb1 >> 16) & 0xFF;
+        int red2 = (rgb2 >> 16) & 0xFF;
+        int red = red2 - red1;
+
+        int green1 = (rgb1 >> 8) & 0xFF;
+        int green2 = (rgb2 >> 8) & 0xFF;
+        int green = green2 - green1;
+
+        int blue1 = (rgb1) & 0xFF;
+        int blue2 = (rgb2) & 0xFF;
+        int blue = blue2 - blue1;
+
         return red * red + green * green + blue * blue;
     }
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        return null;
+        // finding vertical seam for transposed picture == finding horizontal seam for the original one
+        if (isTransposed) {
+            return findVerticalSeam(picture);
+        }
+        return findVerticalSeam(transpose(picture));
     }
 
     private double getWeight(DirectedEdge[][] matrix, int i, int j) {
-        return matrix[i][j] != null ? matrix[i][j].weight() : 0;
+        return matrix[i][j] != null ? matrix[i][j].weight() : Double.POSITIVE_INFINITY;
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        DirectedEdge[][] matrix = new DirectedEdge[height()][width()];
+        if (isTransposed) {
+            return findVerticalSeam(transpose(picture));
+        }
+        return findVerticalSeam(picture);
+    }
+
+    private int[] findVerticalSeam(Picture somePicture) {
+        if (somePicture.width() == 1) {
+            int[] oneColumnArray = new int[somePicture.height()];
+            Arrays.fill(oneColumnArray, 0);
+            return oneColumnArray;
+        }
+        // building paths through energy matrix;
+        // we can move only down, down left or down right from each particular pixel (for vertical carving)
+        // accumulating weight from the previous minimum path, which makes this problem a DP one
+        DirectedEdge[][] matrix = new DirectedEdge[somePicture.height()][somePicture.width()];
         for (int i = 1; i < matrix.length - 1; i++) {
             for (int j = 1; j < matrix[i].length - 1; j++) {
                 double prevLeft = getWeight(matrix, i - 1, j - 1);
                 double prev = getWeight(matrix, i - 1, j);
                 double prevRight = getWeight(matrix, i - 1, j + 1);
-                double current = energy(j, i);
-                if (prev <= prevLeft && prev <= prevRight) {
-                    matrix[i][j] = new DirectedEdge(j, j, prev + current);
-                } else if (prevLeft <= prevRight && prevLeft <= prev) {
-                    matrix[i][j] = new DirectedEdge(j - 1, j, prevLeft + current);
+                double current = energy(somePicture, j, i);
+
+                int from;
+                double weight;
+                if (hasMinWeight(prev, prevLeft, prevRight)) {
+                    from = j;
+                    weight = prev + current;
+                } else if (hasMinWeight(prevLeft, prevRight, prev)) {
+                    from = j - 1;
+                    weight = prevLeft + current;
+                } else if (hasMinWeight(prevRight, prevLeft, prev)) {
+                    from = j + 1;
+                    weight = prevRight + current;
                 } else {
-                    matrix[i][j] = new DirectedEdge(j + 1, j, prevRight + current);
+                    from = j;
+                    weight = current;
                 }
+                matrix[i][j] = new DirectedEdge(from, j, weight);
             }
         }
-        double min = Double.MAX_VALUE;
-        int index = 0;
-        for (int i = 0; i < matrix[0].length - 1; i++) {
-            if (matrix[height() - 1][i].weight() < min) {
-                min = matrix[height() - 1][i].weight();
-                index = i;
-            }
-        }
-        int[] verticalSeam = new int[height()];
-        verticalSeam[verticalSeam.length - 1] = index;
-        for (int i = verticalSeam.length - 2; i >=0; i--) {
+
+        // finding the path with minimum weight
+        int index = somePicture.height() <= 2 ? 0 : IntStream.range(1, matrix[somePicture.height() - 2].length - 1).boxed()
+                .min(Comparator.comparingDouble(i -> matrix[somePicture.height() - 2][i].weight()))
+                .orElse(0);
+
+        // building vertical seam by traversing minimum path edge by edge from the end to the beginning
+        int[] verticalSeam = new int[somePicture.height()];
+        verticalSeam[verticalSeam.length - 1] = index > 0 ? index - 1 : index;
+
+        for (int i = verticalSeam.length - 2; i >= 1; i--) {
             verticalSeam[i] = matrix[i][index].to();
             index = matrix[i][index].from();
         }
+        verticalSeam[0] = index > 0 ? index - 1 : index;
         return verticalSeam;
     }
 
-    // remove horizontal seam from current picture
-    public void removeHorizontalSeam(int[] seam) {
-        if (seam == null) {
-            throw new IllegalArgumentException("Cant be null!");
+    private boolean hasMinWeight(double original, double firstToCompare, double secondToCompare) {
+        return original != Double.POSITIVE_INFINITY && original <= firstToCompare && original <= secondToCompare;
+    }
+
+    private Picture transpose(Picture original) {
+        int height = original.height();
+        int width = original.width();
+
+        // noinspection SuspiciousNameCombination
+        Picture transposed = new Picture(height, width);
+        for (int y = 0; y < transposed.height(); y++) {
+            for (int x = 0; x < transposed.width(); x++) {
+                transposed.set(x, y, original.get(y, x));
+            }
         }
+        this.picture = transposed;
+        this.isTransposed = !this.isTransposed;
+        return transposed;
+    }
+
+    // remove horizontal seam from current picture
+    // remove vertical seam from transposed picture == remove horizontal seam from original one
+    public void removeHorizontalSeam(int[] seam) {
+        Picture pictureToResize = this.picture;
+        if (!isTransposed) {
+            pictureToResize = transpose(this.picture);
+        }
+        removeVerticalSeam(pictureToResize, seam);
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-        if (seam == null) {
-            throw new IllegalArgumentException("Cant be null!");
+        Picture pictureToResize = this.picture;
+        if (isTransposed) {
+            pictureToResize = transpose(this.picture);
         }
+        removeVerticalSeam(pictureToResize, seam);
     }
 
-    //  unit testing (optional)
-    public static void main(String[] args) {
-
+    private void removeVerticalSeam(Picture original, int[] seam) {
+        if (seam == null || seam.length != original.height() || original.width() <= 1) {
+            throw new IllegalArgumentException("Cant perform vertical seam removal");
+        }
+        Picture resized = new Picture(original.width() - 1, original.height());
+        int prev = seam[0];
+        for (int y = 0; y < original.height(); y++) {
+            int current = seam[y];
+            if (Math.abs(current - prev) > 1 || current < 0 || current >= original.width()) {
+                throw new IllegalArgumentException("Invalid seam");
+            }
+            for (int x = 0; x < original.width() - 1; x++) {
+                int xToSet = x;
+                if (x >= current) {
+                    xToSet += 1;
+                }
+                resized.set(x, y, original.get(xToSet, y));
+            }
+            prev = current;
+        }
+        this.picture = resized;
     }
-
 }
